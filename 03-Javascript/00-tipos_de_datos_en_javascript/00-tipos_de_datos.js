97a10553af04d764ff6f6ee67da4e0c8cc6ff9cf
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-30 12:07:34 -0300

    Tipos de datos en Javascript

---
+++ 03-Javascript/00-tipos_de_datos_en_javascript/00-tipos_de_datos.js
@@ -0,0 +1,32 @@
+//Tipos de datos en Javascript
+//-- Tipo de dato String--//
+/* El String es un tipo de dato que contiene caracteres y texto */
+let nombre = "Santiago";
+console.log(nombre); /* En este ejemplo imprimimos un tipo de dato string */
+
+//--Tipo de dato Numerico--//
+let numero = 12;
+console.log(numero); /* En este ejemplo imprimimos el numero de nuestra variable numero */
+
+//--Tipo de dato Boolean--//
+let boolea = true;
+console.log(boolea); /* En este ejemplo imprimimos el valor boolean */
+
+//--Tipo de dato Date--//
+let data = new Date();
+console.log(data); /* En este ejemplo imprimimos el valor date */
+
+//--Tipo de dato Array--//
+let array = [1, 2, 3];
+console.log(array); /* En este ejemplo imprimimos el valor array */
+
+//--Tipo de dato Object--//
+let objeto = {
+    nombre: "Santiago",
+    numero: 12,
+    boolea: true,
+    data: new Date(),
+    array: [1, 2, 3]
+}
+console.log(objeto); /* En este ejemplo imprimimos el objeto
+/* Con un objeto podemos contener varias propiedades */
