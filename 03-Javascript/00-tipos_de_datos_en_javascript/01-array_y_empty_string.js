b6b247898b3d6a4ebf5d80b2c96a5785b3d81ed4
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-30 20:56:19 -0300

    Tipo de dato Array en Javascript

---
+++ 03-Javascript/00-tipos_de_datos_en_javascript/01-array_y_empty_string.js
@@ -0,0 +1,5 @@
+//--- Tipo de dato Array ---//
+//En un array podemos guardar varios datos del mismo tipo dentro de una variable
+var arrayAutos = ['BMW','Audi','Volvi'];
+console.log(arrayAutos);//Imprimimos todos los elementos dentro del array
+console.log(typeof arrayAutos);//Los array en Javascript son datos de tipo Objeto
