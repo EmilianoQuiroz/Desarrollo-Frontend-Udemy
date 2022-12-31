94dd831f483c434f99c21be9e672b628eb80f332
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-30 21:16:50 -0300

    Let var y const en Javascript

---
+++ 03-Javascript/00-tipos_de_datos_en_javascript/02-let_var_y_const.js
@@ -0,0 +1,16 @@
+//****** Let Var y Const en Javascript *******/
+
+//--- VAR ---//
+//var es una palabra reservada para declarar variables, aunque en la actualidad esta se encuentra en desuso
+//Una practica muy recomendad es usar let
+var nombre;//Podemos declarar una variable sin asignarle ningun valor
+nombre = "Santiago";//Luego podemos usar el nombre de esa variable y asignarle el valor que querramos
+console.log(nombre);//Mostramos en pantalla el valor de la variable
+
+//--- LET ---//
+let apelido = "perez";//Forma correcta de crear variables en la actualidad
+console.log(apelido);
+
+//--- CONST ---//
+//const la usamos para declarar constantes, es decir, contenedores para guardar datos que no pueden cambiar su valor inicial
+const constante = "Developer";//Esta constante no cambiara su valor  
