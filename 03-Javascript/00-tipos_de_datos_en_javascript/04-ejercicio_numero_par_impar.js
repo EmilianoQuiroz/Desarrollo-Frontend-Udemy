df29cfa045aebf6e02bab9e52bbb206beb39b095
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-30 22:17:56 -0300

    Ejercicio numero par o impar en Javascript

---
+++ 03-Javascript/00-tipos_de_datos_en_javascript/04-ejercicio_numero_par_impar.js
@@ -0,0 +1,15 @@
+/*
+EJERCICIO:
+Pedirle al usuario que ingrese dos numero por consola y posteriormente indicarle si este es par o impar
+*/
+
+let numero = 23;//Inicializamos la variable con el numero que querramos
+
+if (numero %= 0)//Si el resto de la variable es 0 entonces es par y ejecutamos el siguiente console
+{
+    console.log("Es par ");
+}
+else// Si el resto de la variable no es 0 entonces es par y ejecutamos el siguiente console
+{
+    console.log("No es par ");
+}
