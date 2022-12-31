e62abbefbe22a845b2cf28fa305338941571f49d
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-30 21:39:02 -0300

    Operadores en Javascript

---
+++ 03-Javascript/00-tipos_de_datos_en_javascript/03-operadores.js
@@ -0,0 +1,40 @@
+//***** Operadores en Javascript ******/
+//Declaramos los valores que vamos a usar en todos los ejemplos
+let a = 2;
+let b = 10;
+
+//--- SUMA ---//
+let suma = a + b;
+console.log(suma);
+
+//--- RESTA ---//
+let resta = a - b;
+console.log(resta);
+
+//--- MULTIPLICACION ---//
+let mult = a * b;
+console.log(mult);
+
+//--- DIVISION ---//
+let div = a / b;
+console.log(div);
+
+//--- MODULO ---//
+let mod = a % b;//Resto de la divicion entre dos numeros
+console.log(mod);
+
+//--- INCREMENTO ---//
+//PreIncremento (el operador ++ antes de la variable)
+let PreIncremento = ++a;
+console.log(a);
+//PostIncremento (el operador ++ despues de la variable)
+let Proincremento = a++;
+console.log(a);
+
+//--- DECREMENTO ---//
+//PreDecremento
+let preDecremento = --a;
+console.log(a);
+//PostDecremento
+let postDecremento = a--;
+console.log(a); 
