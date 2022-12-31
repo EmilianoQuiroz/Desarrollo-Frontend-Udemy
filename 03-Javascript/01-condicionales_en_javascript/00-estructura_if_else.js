3ce75640e09afbc1f153d501c944f8927f9c3cd9
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-12-31 15:31:00 -0300

    Estructura IF ELSE en Javascript

---
+++ 03-Javascript/01-condicionales_en_javascript/00-estructura_if_else.js
@@ -0,0 +1,12 @@
+/****** ESTRUCTURA IF ELSE ******/
+
+let condicion = true;//Condicion a cumplir
+
+if (condicion == true)//En caso de que condicion sea true se imprimira el siguiente console
+{
+    console.log("el valor del condición es: true");
+}
+else//Si la condicion es false se imprimir el siguiente console
+{
+    console.log("el valor del condición es: false");
+}
