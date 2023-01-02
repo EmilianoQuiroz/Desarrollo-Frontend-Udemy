a43bc7b84c58e414ecccf94ad991b4b6a1f81c1d
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-02 20:02:12 -0300

    Return en Javascript

---
+++ 03-Javascript/04-funciones;_en_javascript/01-return.js
@@ -0,0 +1,16 @@
+//***** RETURN *****//
+
+//Declaracion de la funcion
+function miFuncion(a,b)
+{
+
+    return a + b;//Ahora la funcion retorna la suma de los parametros, con lo cual ahora podemos usar la funcion dentro de una variable
+
+}
+
+//Llamado a la funcion
+let resultado = miFuncion(4,6);
+
+//Ahora imprimimos la variable que contienne el resultado de nuestra funcion
+
+console.log(resultado);
