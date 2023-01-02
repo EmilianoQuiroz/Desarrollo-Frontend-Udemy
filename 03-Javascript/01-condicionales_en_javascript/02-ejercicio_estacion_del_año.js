99191be1ffb4b5a6dc5303bdab52d93fc520c561
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-02 00:58:41 -0300

    Ejercicio meses del año en Javascript

--- 03-Javascript/01-condicionales_en_javascript/02-ejercicio_estacion_del_año.js
+++ 03-Javascript/01-condicionales_en_javascript/02-ejercicio_estacion_del_año.js
@@ -1,2 +1,20 @@
 //***** EJERCICIO *****//
 //Pedirle al usuario que digite un mes del año e indicarle en cual estacion del año se encuentra
+
+let mes = "Marzo";
+
+if (mes == "Diciembre" || mes == "Enero" || mes == "Febrero")
+{
+    console.log("Verano");
+}
+else if(mes == "Marzo" || mes == "Abril" || mes == "Mayo")
+{
+    console.log("Otoño");
+}
+else if(mes == "Junio" || mes == "Julio" || mes == "Agosto")
+{
+    console.log("Invierno");
+}
+else{
+    console.log("Primavera");
+}
