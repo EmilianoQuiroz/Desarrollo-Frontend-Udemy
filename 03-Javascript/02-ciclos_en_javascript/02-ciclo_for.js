8746ba1bb0aac3200bc790ee0ed036bed8d1a30a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-02 04:03:30 -0300

    Ciclo For en Javascript

---
+++ 03-Javascript/02-ciclos_en_javascript/02-ciclo_for.js
@@ -0,0 +1,11 @@
+//***** CICLO FOR *****//
+
+//Un ciclo for se repite hasta que una condición especificada se evalúe como false . El bucle for de JavaScript es similar al bucle for de Java y C. Cuando se ejecuta un bucle for , ocurre lo siguiente: Se ejecuta la expresión de iniciación expresiónInicial , si existe.
+
+for (let i = 1; i <= 10; i++) 
+{
+    /* En este ciclo podemos declarar el contador y asignarele un calor dentro del mismo ciclo */
+    /* En este caso i es igual a uno, mientras i sea menor que 10 se ejecutara el ciclo sumandole una unidad a y (i++) */
+    console.log(i);/* Imprimimos el valor de "i" */
+}
+
