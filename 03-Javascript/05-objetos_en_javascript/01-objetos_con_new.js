40e8fdd11eef7f0fe0da9b0c531700b077127371
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-03 09:53:09 -0300

    Objetos con New en Javascript

---
+++ 03-Javascript/05-objetos_en_javascript/01-objetos_con_new.js
@@ -0,0 +1,7 @@
+//***** OBJETOS CON NEW *****//
+/* Con la palabra new se le asigna un nuevo espacio en memoria a nuestro objeto */
+let persona = new Object();/* Primero creamos el objeto vacio */
+/* Luego le asignamos sus propiedades */
+persona.nombre = 'Carlos';
+persona.apellido = 'Adela';
+persona.email = '123abc@gmail.com';
