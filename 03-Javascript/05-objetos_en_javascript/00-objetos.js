8b8347e21bc186f53f52e09ac95206070ead3110
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-03 09:46:46 -0300

    Objetos en Javascript

--- 03-Javascript/05-objetos_en_javascript/00-objetos.js
+++ 03-Javascript/05-objetos_en_javascript/00-objetos.js
@@ -6,13 +6,18 @@
 //Primero definimos la variable y luego, entre corchetes, ponemos las propiedades
 let persona = {
     name: "John",
+    surname: "Doe",
     age: 30,
     hobby: "coding",
-    email: "john.doe@gmail.com"
+    email: "john.doe@gmail.com",
+    nombreCompleto: function() {
+        return this.nombre;
+    }
 }
 
 //Acceder a nuestro objeto 
-console.log(persona);
+// console.log(persona);
 //Acceder a una propiedad de nuestro objeto
-console.log(persona.age);
-console.log(persona.name);
+// console.log(persona.age);
+// console.log(persona.name);
+console.log(nombreCompleto());
