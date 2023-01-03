03eea73d7e1ade4d9b1239fc61114b9f8137fdd0
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-03 09:34:39 -0300

    Objetos en Javascript

---
+++ 03-Javascript/05-objetos_en_javascript/00-objetos.js
@@ -0,0 +1,18 @@
+//***** OBJETOS *****//
+
+// En JavaScript, un objeto es un entidad independiente con propiedades y tipos.
+
+// Estructura de un objeto
+//Primero definimos la variable y luego, entre corchetes, ponemos las propiedades
+let persona = {
+    name: "John",
+    age: 30,
+    hobby: "coding",
+    email: "john.doe@gmail.com"
+}
+
+//Acceder a nuestro objeto 
+console.log(persona);
+//Acceder a una propiedad de nuestro objeto
+console.log(persona.age);
+console.log(persona.name);
