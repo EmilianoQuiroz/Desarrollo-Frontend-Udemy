a3bbeb90d355fbd70b4dbee88729c0897f3c8471
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-03 18:12:34 -0300

    Metodo Set en Javascript

--- 03-Javascript/05-objetos_en_javascript/02-metodos_get_y_set.js
+++ 03-Javascript/05-objetos_en_javascript/02-metodos_get_y_set.js
@@ -14,3 +14,28 @@ let persona = {
 //Anteponiendo get en nuetra funcion podemos simplificar la sintaxis de la llamanada a la funcion sacandole los parentesis 
 
 console.log(persona.nombreCompleto);
+
+// SET
+
+let persona2 = {
+    nombre: 'Santiago',
+    apellido: 'Quiroz',
+    email: 'Santiago.Quiroz@gmail.com',
+    idioma: 'es',
+    edad: '24',
+    get lang(){
+        return this.idioma.toLocaleUpperCase();
+    },
+    set lang(lang){
+        this.idioma = lang.toLocaleUpperCase();
+    },
+    get nombreCompleto(){
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+
+console.log(persona2.lang);
+
+persona2.lang = 'en';
+
+console.log(persona2.lang);
