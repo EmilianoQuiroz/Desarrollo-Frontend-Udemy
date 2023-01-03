9b45c0c90e621c4f82a221a38a8376110aa693e8
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-03 01:03:02 -0300

    Funciones anonimas en Javascript

---
+++ 03-Javascript/04-funciones_en_javascript/02-funcion_anonima.js
@@ -0,0 +1,11 @@
+//***** FUNCIONES ANONIMAS *****/
+// Las funciones anónimas en JavaScript son aquellas que no han sido declaradas con un nombre. Estas declaradas dentro de una variable
+
+// Declaramos la funcion
+let sumar = function(a,b){ return a+b};
+
+//Llamamos a la funcion 
+resultado = sumar(12,34);
+
+//Mostramos el resltado de la variable resultado
+console.log(resultado);
