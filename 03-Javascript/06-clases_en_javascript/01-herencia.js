953d0ecad75a1b8d50a34fc86009328a4417d375
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-09 10:27:11 -0300

    Herencia en Javascript

---
+++ 03-Javascript/06-clases_en_javascript/01-herencia.js
@@ -0,0 +1,42 @@
+//***** Herencia en Javascript *****//
+// La herencia en JavaScript es un concepto que nos permite hacer que un objeto herede los comportamientos de otro, al igual que lo harían las características de un ser vivo en la vida real. Además, podemos ir añadiendo comportamientos en la línea de herencia para que cada objeto tenga características únicas o nuevas.
+
+//Clase padre
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this.apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set apellido(nombre){
+        this._apellido = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+//Clase hija
+class Empleado extends Persona{
+    constructor(nombre, apellido, area){
+        super(nombre, apellido);
+        this._area = area;
+    }
+    get area(){
+            return this._area;
+        }
+        set area(area){
+            this._area = area;
+        }
+}
+
+//Creamos un nuevo objeto con la clase hija 
+let empleado1 = new Empleado('Santiago', 'Quiroz', 'It');
+
+//Imprimimos los valores de la clase hija
+console.log(empleado1);
