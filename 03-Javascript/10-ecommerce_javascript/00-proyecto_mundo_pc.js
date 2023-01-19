de509a1a8c3ac75ed63f228ba4e2e8df2f7ade38
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-19 11:54:18 -0300

    Clase hija Raton ejercicio Mundo PC en Javascript

--- 03-Javascript/10-ecommerce_javascript/00-proyecto_mundo_pc.js
+++ 03-Javascript/10-ecommerce_javascript/00-proyecto_mundo_pc.js
@@ -2,3 +2,48 @@
 /*
 Crear una aplicacion que tenga una clase padre llamada DispositivoEntrada que tenga dos clases hijas, una llamada Raton y la otra Teclado. La clase Teclado a su vez debe tener una clase hija llamada Monitor, de esta se extiende la calse Computadora que se compone de todos los objetos creados hasta el momento. Por ultimo, se crea la clase Orden, con la cual, debemos poder crear una serie de computaodras.
 */
+
+//Clase padre: DispositivoEntrada
+class DispositivoEntrada{
+    constructor(tipoEntrada, marca){
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+    get marca(){
+        return this._marca;
+    }
+    set marca(marca){
+        this._marca = marca;
+    }
+}
+
+//Clases hijas
+//Clase Raton
+class Raton extends DispositivoEntrada{
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+    get idRaton(){
+        return this._idRaton;
+    }
+    toString(){
+        return `Raton:[idRaton: ${this._idRaton}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}]`;
+    }
+}
+
+//Prueba clase raton
+/*
+let raton1 = new Raton('USB', 'HP');
+console.log(raton1.toString());
+let raton2 = new Raton('Bluetooth', 'Red Dragon');
+console.log(raton2.toString());
+*/
