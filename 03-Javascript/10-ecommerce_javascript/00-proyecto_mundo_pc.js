bf8bcd3d97ffdc620bd072344d410a1b07464ddc
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-19 10:53:41 -0300

    Consigna ejercicio Mundo PC en Javascript

---
+++ 03-Javascript/10-ecommerce_javascript/00-proyecto_mundo_pc.js
@@ -0,0 +1,4 @@
+//***** Proyecto Mundo PC en Javascript *****//
+/*
+Crear una aplicacion que tenga una clase padre llamada DispositivoEntrada que tenga dos clases hijas, una llamada Raton y la otra Teclado. La clase Teclado a su vez debe tener una clase hija llamada Monitor, de esta se extiende la calse Computadora que se compone de todos los objetos creados hasta el momento. Por ultimo, se crea la clase Orden, con la cual, debemos poder crear una serie de computaodras.
+*/
