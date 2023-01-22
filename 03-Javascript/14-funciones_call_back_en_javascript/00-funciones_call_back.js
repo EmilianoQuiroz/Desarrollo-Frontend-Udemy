91c2bc83c973ac2dbd9133057bbd61ca43e1c16e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-21 22:46:42 -0300

    Funciones Call back en Javascript

---
+++ 03-Javascript/14-funciones_call_back_en_javascript/00-funciones_call_back.js
@@ -0,0 +1,3 @@
+//***** Funciones Callback en Javascript *****//
+//Una función de callback es una función que se pasa a otra función como un argumento, que luego se invoca dentro de la función externa para completar algún tipo de rutina o acción. El ejemplo anterior es una callback sincrónica, ya que se ejecuta inmediatamente.
+
