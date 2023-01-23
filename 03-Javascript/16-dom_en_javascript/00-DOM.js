a56afe09056942265918225a608da69ddae1f6d2
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-23 11:20:24 -0300

    Accediendo al DOM en Javascript

---
+++ 03-Javascript/16-dom_en_javascript/00-DOM.js
@@ -0,0 +1,11 @@
+//***** Document Objet Model en Javascript *****//
+//alert('Hola desde JS');
+
+//Accedemos a los elementos definidos en el documento html
+let cabecero = document.getElementById('cabecero').innerHTML;
+let parrafo = document.getElementById('parrafo').innerHTML;
+
+
+//Imprimimos en patalla los elementos
+document.write(cabecero);
+document.write(parrafo);
