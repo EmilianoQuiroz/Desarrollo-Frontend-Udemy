1bad8868de6b8805eaa88d938399a1250e48f179
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-25 20:52:21 -0300

    Onchage en Javascript

---
+++ 03-Javascript/17-eventos_en_javascript/02-onchange.js
@@ -0,0 +1,6 @@
+//***** Onchage en Javascript *****//
+
+function convertir(nombreInput){
+    /*Todos los elementos que entren dentro del input pasaran a escribirse automaticamente en mayuscula*/
+    nombreInput.value = nombreInput.value.toUpperCase();
+}
