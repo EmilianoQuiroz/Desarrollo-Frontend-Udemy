26802a5caaaea17b5f227929222bc363f99f814f
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-05 21:32:18 -0300

    Resto de las funciones

--- 04-Proyectos/00-calculadora_javascript/calculadora.js
+++ 04-Proyectos/00-calculadora_javascript/calculadora.js
@@ -18,3 +18,66 @@
     //Mostramos el resultado de la operacion en el div
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
 }
+
+//Funcion de Resta
+function restar (){
+
+    //Definimos una constante para recibir el formulario
+    const forma = document.getElementById('forma');
+    
+    //Recuperamos los elementos operandoA y operandoB
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    
+    //Definimos el resultado de la resta de los dos operandos
+    let resultado = operandoA.value - operandoB.value;
+
+    //En caso de que el usuario no digite un numero ejecutamos el siguiente if
+    if(isNaN(resultado)){
+        resultado = 'Digite un numero';
+    }
+    //Mostramos el resultado de la operacion en el div
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
+
+//Funcion de Multiplicar
+function multiplicar (){
+
+    //Definimos una constante para recibir el formulario
+    const forma = document.getElementById('forma');
+    
+    //Recuperamos los elementos operandoA y operandoB
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    
+    //Definimos el resultado de la multiplicacion de los dos operandos
+    let resultado = operandoA.value * operandoB.value;
+
+    //En caso de que el usuario no digite un numero ejecutamos el siguiente if
+    if(isNaN(resultado)){
+        resultado = 'Digite un numero';
+    }
+    //Mostramos el resultado de la operacion en el div
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
+
+//Funcion de Division
+function dividir (){
+
+    //Definimos una constante para recibir el formulario
+    const forma = document.getElementById('forma');
+    
+    //Recuperamos los elementos operandoA y operandoB
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    
+    //Definimos el resultado de la division de los dos operandos
+    let resultado = operandoA.value / operandoB.value;
+
+    //En caso de que el usuario no digite un numero ejecutamos el siguiente if
+    if(isNaN(resultado)){
+        resultado = 'Digite un numero';
+    }
+    //Mostramos el resultado de la operacion en el div
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
