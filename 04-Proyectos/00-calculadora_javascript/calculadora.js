a85399fab484be3d9f931c218eb9a5675d423630
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-05 21:08:05 -0300

    Operacion Sumar

---
+++ 04-Proyectos/00-calculadora_javascript/calculadora.js
@@ -0,0 +1,20 @@
+//Funcion de Suma
+ function sumar (){
+
+    //Definimos una constante para recibir el formulario
+    const forma = document.getElementById('forma');
+    
+    //Recuperamos los elementos operandoA y operandoB
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    
+    //Definimos el resultado de la suma de los dos operandos y los parseamos
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+
+    //En caso de que el usuario no digite un numero ejecutamos el siguiente if
+    if(isNaN(resultado)){
+        resultado = 'Digite un numero';
+    }
+    //Mostramos el resultado de la operacion en el div
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
