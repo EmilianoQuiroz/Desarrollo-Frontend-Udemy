62f9d98ec53d04410e58da8eb982873143388dd1
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-07 20:14:33 -0300

    Seccion Egresos JS

--- 04-Proyectos/03-aplicacion_presupuesto_javascript/js/app.js
+++ 04-Proyectos/03-aplicacion_presupuesto_javascript/js/app.js
@@ -5,12 +5,15 @@ const ingresos = [
 
 const egresos = [
     new Egreso('Renta departamento', 900),
+    new Egreso('Ropa', 400),
+    new Egreso('Ropa', 400),
     new Egreso('Ropa', 400)
 ];
 
 let cargarApp = ()=>{
     cargarCabecero();
     cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = ()=>{
@@ -46,6 +49,7 @@ const formatoPorcentaje = (valor) => {
     return valor.toLocaleString('en-US',{style:'percent', minimumFractionDigitn:2})
 }
 
+//Cargar Ingresos
 const cargarIngresos = ()=>{
     let ingresosHTML = '';
     for(let ingreso of ingresos){
@@ -70,4 +74,31 @@ const crearIngresoHTML = (ingreso)=>{
                 </div>
     `;
     return ingresoHTML;
+}
+
+//Cargat Egresos
+const cargarEgresos = ()=>{
+    let egresosHTML = '';
+    for(let egreso of egresos){
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+}
+
+const crearEgresoHTML = (egreso)=>{
+    let egresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
+        <div class="elemento_eliminar">
+            <button class='elemento_eliminar--btn'>
+                <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+    `;
+    return egresoHTML;
 }
