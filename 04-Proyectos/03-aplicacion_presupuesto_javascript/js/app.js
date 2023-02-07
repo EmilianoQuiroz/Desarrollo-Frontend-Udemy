fc1493ff693e80e4a1a68fb9ceda86f51fdef28e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-07 11:23:52 -0300

    Creacion de arreglos de Ingresos y Egresos

---
+++ 04-Proyectos/03-aplicacion_presupuesto_javascript/js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Salario', 2100),
+    new Ingreso('Venta Coche', 1500)
+];
+
+const egresos = [
+    new Egreso('Pago de alquiler', 900),
+    new Egreso('Spotify', 50)
+];
+
+let cargarApp = () => {
+    cargarCabecero();
+}
+
+let cargarCabecero = () => {
+    
+}
